2c36309bc6a49c311343698e61ad318f217e2b6a
Paweł Wojciechowski <pwojciech0@wp.pl>
2019-07-07 23:31:04 +0200

    config db

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://Arni24:UIiWiIkzpCGpVkrx@cluster0-pwnow.mongodb.net/test?retryWrites=true&w=majority',
     keySession: ['TWOJKLUCZ'],
     maxAgeSession: 24 * 60 * 60 * 1000
 }
